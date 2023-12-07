255a91bb063eb2486e3091c2580ccdefb8242344
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2023-12-07 18:13:24 -0500

    can retrieve bio and preparation times

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,49 @@ struct Recipe {
     title: String, //span.card__title-text
     image: String, //img data-src
     bio: String, // id = article-heading_1-0
+    times: String,
 }
 
 #[derive(Debug)]
 struct RecipeDetail {
-    // times: Option<String>, // div.mntl-recipe-details__value
+    times: String, // div.mntl-recipe-details__value"
     // ingredients: Option<String>, // li class = mntl-structured-ingredients__list-item, then its within the ps
     bio: String, // id = article-heading_1-0
 }
 
 
-
-
-
 fn main() {
 
-    // Get recipe details 
+    // This function is responsible for going into each recipe's url and retrieving the 
+    // bio, preparation times, and the ingredients.
     fn fetchDetails(url: String) -> RecipeDetail {
         let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-        //Get response and convert to html tree
-    
-        //Below converts to an html tree that's easier to parse
         let document = scraper::Html::parse_document(&response);
 
+        //Retrieivng bio
         let bio_selector = scraper::Selector::parse("p#article-subheading_1-0").unwrap();
         let bio_contents = document.select(&bio_selector).next().map(|x| x.inner_html());
         let bio = match bio_contents {
-            None => "NO bio",
+            None => "No bio",
             Some(ref x) => x,
         }.to_string();
 
-        // let ingr_selector = scraper::Selector::parse("li.mntl-structured-ingredients__list-item");
+        //Retrieving time
+        let times_selector = scraper::Selector::parse("div.mntl-recipe-details__value").unwrap();
+        let times_string = document.select(&times_selector).map(|x| x.inner_html());
+        let mut times = "".to_owned();
+        times_string
+            .zip(1..5)
+            .for_each(|(item, id)| {
+                println!("{}, {}", item, id);
+                times.push_str(&item);
+        });
+        
+
         // let ingredients = document.select(&ingr_selector).next().map(|x| x.inner_html());
 
-        // let times_selector = scraper::Selector::parse("div.mntl-recipe-details__value");
-        // let times = document.select(&times_selector).next().map(|x| x.inner_html());
-
         let new_recipe_details = RecipeDetail {
+            times,
             bio,
         };
         return new_recipe_details;
@@ -53,7 +59,8 @@ fn main() {
 
 
 
-    // Get target webpage and parse into html
+    // This function is responsible for scraping the allrecipes ingredients page, and retrieving a list of recipes that
+    // contains the recipe title, url and image.
     fn fetchData(url: String, fileName: String) {
 
         let response = reqwest::blocking::get(url);
@@ -94,18 +101,19 @@ fn main() {
             }.to_string();
     
 
-            //Fetch specific details and convert to string
+            //Here we call our fetchDetails function, and grab more details (bio, prep times) from the recipe's 
+            //actual url to add to our recipe object.
             let crawler_url = url.clone();
             let recipe_details = fetchDetails(crawler_url);  //call function with url
-            // let times = recipe_details.times;
-            // let ingredients = recipe_details.ingredients;
             let bio = recipe_details.bio;
+            let times = recipe_details.times;
   
             let new_recipe = Recipe {
                 url, 
                 title,
                 image,
                 bio,
+                times,
             };
             recipes.push(new_recipe);
         }
@@ -113,15 +121,16 @@ fn main() {
         let path = std::path::Path::new(&pathStr);
         let mut writer = csv::Writer::from_path(path).unwrap();
 
-        writer.write_record(&["url","title","image", "bio"]).unwrap();
+        writer.write_record(&["url","title","image", "bio", "times"]).unwrap();
 
         for recipe in recipes {
             let url = recipe.url;
             let title = recipe.title;
             let image = recipe.image;
             let bio = recipe.bio;
+            let times = recipe.times;
 
-            writer.write_record(&[url,title,image, bio]).unwrap();
+            writer.write_record(&[url,title,image, bio, times]).unwrap();
         }
         writer.flush().unwrap();
     }
